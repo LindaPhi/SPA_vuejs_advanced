bb705936408939f37dcc299e0c2bf06142ac130d
LindaPhi <linda.phinouvong@le-campus-numerique.fr>
2021-02-18 15:57:04 +0100

    v2 >v3

--- src/App.vue
+++ src/App.vue
@@ -17,49 +17,48 @@ export default {
   name: 'App'
 }
 </script>
-
 <style>
 html, body {
-  margin: 0;
-  padding: 0;
-}
+    margin: 0;
+    padding: 0;
+  }
 
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #262626;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #262626;
+  }
 
-#app_header {
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  background-color: #262626;
-  padding-right: 20px;
-}
+  #app_header {
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    background-color: #262626;
+    padding-right: 20px;
+  }
 
-#app_header img {
-  height: 80px;
-  padding: 20px;
-}
+  #app_header img {
+    height: 80px;
+    padding: 20px;
+  }
 
-#app_header a {
-  color: white;
-  text-transform: uppercase;
-  padding: 20px;
-  text-decoration: none;
-}
+  #app_header a {
+    color: white;
+    text-transform: uppercase;
+    padding: 20px;
+    text-decoration: none;
+  }
 
-#map {
-  width: 100%;
-  height: 500px;
-}
+  #map {
+    width: 100%;
+    height: 500px;
+  }
 
-#map .marker {
-  width: 50px;
-  height: auto;
-  cursor: pointer;
-}
+  #map .marker {
+    width: 50px;
+    height: auto;
+    cursor: pointer;
+  }
 </style>
